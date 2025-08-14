c68f7c23c4f48e722aed7eac68b77b27f23178a2
nessmp <nestormp27@gmail.com>
2025-08-14 14:54:09 -0600

    Add constant file for controllers and labels

---
+++ lib/widgets/constants.dart
@@ -0,0 +1,31 @@
+class Constants {
+  static const double kWindowWidth = 1024;
+  static const double kWindowHeight = 800;
+  static const String kWindowTitle = 'Notaria 27'; 
+
+  static const String kBirthDateLabel = 'Fecha de Nacimiento';
+  static const String kCityLabel = 'Ciudad';
+  static const String kColoniaLabel = 'Colonia';
+  static const String kCurpLabel = 'CURP';
+  static const String kEconomicActivitiesLabel = 'Actividades Económicas';
+  static const String kHouseNumberLabel = 'Numero Exterior';
+  static const String kNameLabel = 'Nombre';
+  static const String kRegimesLabel = 'Regimenes';
+  static const String kRfcLabel = 'RFC';
+  static const String kStateLabel = 'Estado';
+  static const String kStreetLabel = 'Calle';
+  static const String kZipCodeLabel = 'Codigo Postal';
+
+  static const String kCsfBirthDate = 'get_birth_date';
+  static const String kCsfCity = 'get_city';
+  static const String kCsfColonia = 'get_colonia';
+  static const String kCsfCurp = 'get_curp';
+  static const String kCsfEconomicActivities = 'get_economic_activities';
+  static const String kCsfHouseNumber = 'get_house_number';
+  static const String kCsfName = 'get_name';
+  static const String kCsfRegimes = 'get_regimes';
+  static const String kCsfRfc = 'get_rfc';
+  static const String kCsfState = 'get_state';
+  static const String kCsfStreet = 'get_street';
+  static const String kCsfZipCode = 'get_zip_code';
+}
